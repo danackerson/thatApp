a6fa59687a8addd2bf7a6905adc9cb089aca4b77
Dan Ackerson <dan@ackerson.de>
2015-11-19 12:05:00 +0100

    try intercontainer communication straight thru docker network

--- hello_that.go
+++ hello_that.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,8 +11,25 @@ import (
 func main() {
 	http.HandleFunc("/", hello)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	response, err := http.Get("http://this:8080/")
+	checkError(err)
+	contents := readBody(response.Body)
+	fmt.Println(contents)
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello That!")
+}
+
+func readBody(body io.ReadCloser) []byte {
+	defer body.Close()
+	contents, err := ioutil.ReadAll(body)
+	checkError(err)
+	return contents
+}
+func checkError(err error) {
+	if err != nil {
+		fmt.Printf("%s", err)
+		//os.Exit(1)
+	}
 }
